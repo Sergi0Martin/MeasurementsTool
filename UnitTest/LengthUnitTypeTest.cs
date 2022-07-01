b2ab9d84ee8dfea4b705b57e5c5f3168b53c2e41
Sergio Martin <smartin@logisfashion.com>
2022-07-01 08:10:57 +0200

    Added intial tests structure

---
+++ UnitTest/LengthUnitTypeTest.cs
@@ -0,0 +1,16 @@
+namespace UnitTest
+{
+    public class LengthUnitTypeTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ UnitTest/WeightUnitTypeTest.cs
@@ -0,0 +1,16 @@
+namespace UnitTest
+{
+    public class WeightUnitTypeTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
