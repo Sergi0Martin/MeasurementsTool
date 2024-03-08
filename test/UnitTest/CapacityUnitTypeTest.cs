0675276f06412685c7f4cbe1b688a301f4102f63
Sergio <16337833+Sergi0Martin@users.noreply.github.com>
2024-03-08 02:57:17 +0100

    Add CapacityUnit and new docs format (#5)

---
+++ test/UnitTest/CapacityUnitTypeTest.cs
@@ -0,0 +1,25 @@
+﻿namespace MeasurementsTool.UnitTests;
+
+public sealed class CapacityUnitTest
+{
+    [SetUp]
+    public void Setup() { }
+
+    [Theory]
+    [TestCase(CapacityUnitType.L, CapacityUnitType.mL, 50.5, 50500)]
+    [TestCase(CapacityUnitType.mL, CapacityUnitType.L, 50.5, 0.0505)]
+    [TestCase(CapacityUnitType.YL, CapacityUnitType.PL, 50.5, 50500000000)]
+    [TestCase(CapacityUnitType.YL, CapacityUnitType.yL, 50.5, 5.05E+47)]
+    public void ConvertTo_International_System_Works(CapacityUnitType from, CapacityUnitType to, double value, double expectedResult)
+    {
+        // Act
+        var conversion = from.ConvertTo(
+            to: to,
+            value: value);
+
+        // Assert
+        conversion
+            .Should()
+            .Be(expectedResult);
+    }
+}
--- test/UnitTest/LengthUnitTypeTest.cs
+++ test/UnitTest/LengthUnitTypeTest.cs
@@ -1,42 +1,41 @@
-namespace UnitTest
+namespace MeasurementsTool.UnitTests;
+
+public sealed class LengthUnitTypeTest
 {
-    public class LengthUnitTypeTest
+    [SetUp]
+    public void Setup() { }
+
+    [Theory]
+    [TestCase(LengthUnitType.M, LengthUnitType.MM, 50.5, 50500)]
+    [TestCase(LengthUnitType.MM, LengthUnitType.DM, 50.5, 0.505)]
+    [TestCase(LengthUnitType.KM, LengthUnitType.M, 50.5, 50500)]
+    public void ConvertTo_International_System_Works(LengthUnitType from, LengthUnitType to, double value, double expectedResult)
     {
-        [SetUp]
-        public void Setup() { }
+        // Act
+        var conversion = from.ConvertTo(
+            to: to,
+            value: value);
 
-        [Theory]
-        [TestCase(LengthUnitType.M, LengthUnitType.MM, 50.5, 50500)]
-        [TestCase(LengthUnitType.MM, LengthUnitType.DM, 50.5, 0.505)]
-        [TestCase(LengthUnitType.KM, LengthUnitType.M, 50.5, 50500)]
-        public void ConvertTo_International_System_Works(LengthUnitType from, LengthUnitType to, double value, double expectedResult)
-        {
-            // Act
-            var conversion = from.ConvertTo(
-                to: to,
-                value: value);
+        // Assert
+        conversion
+            .Should()
+            .Be(expectedResult);
+    }
 
-            // Assert
-            conversion
-                .Should()
-                .Be(expectedResult);
-        }
+    [Theory]
+    [TestCase(LengthUnitType.YD, LengthUnitType.MM, 50.5, 46177.2)]
+    [TestCase(LengthUnitType.FT, LengthUnitType.IN, 50.5, 606)]
+    [TestCase(LengthUnitType.IN, LengthUnitType.M, 50.5, 1.2827)]
+    public void ConvertTo_Non_International_System_Works(LengthUnitType from, LengthUnitType to, double value, double expectedResult)
+    {
+        // Act
+        var conversion = from.ConvertTo(
+            to: to,
+            value: value);
 
-        [Theory]
-        [TestCase(LengthUnitType.YD, LengthUnitType.MM, 50.5, 46177.2)]
-        [TestCase(LengthUnitType.FT, LengthUnitType.IN, 50.5, 606)]
-        [TestCase(LengthUnitType.IN, LengthUnitType.M, 50.5, 1.2827)]
-        public void ConvertTo_Non_International_System_Works(LengthUnitType from, LengthUnitType to, double value, double expectedResult)
-        {
-            // Act
-            var conversion = from.ConvertTo(
-                to: to,
-                value: value);
-
-            // Assert
-            conversion
-                .Should()
-                .Be(expectedResult);
-        }
+        // Assert
+        conversion
+            .Should()
+            .Be(expectedResult);
     }
 }
